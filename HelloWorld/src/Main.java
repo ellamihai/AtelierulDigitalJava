e933a99122b0c47061155b546c9469069f2589e4
ellamihai <mihaela_mihaimihaela@yahoo.com>
2018-11-12 17:22:57 +0200

    print

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String s = " java ";
+        s=s.concat("code");
+        System.out.println(s);
+    }
+}
